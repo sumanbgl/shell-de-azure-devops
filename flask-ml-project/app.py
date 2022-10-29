db02f8082f9309b01e50bc02d4e0605f4d8c2a92
Suman <suman.bgl@gmail.com>
2022-10-29 12:52:49 +0000

    Fixed errors.

--- flask-ml-project/app.py
+++ flask-ml-project/app.py
@@ -3,7 +3,8 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
-import joblib
+#import joblib
+from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
@@ -14,7 +15,7 @@ def scale(payload):
     """Scales Payload"""
 
     LOG.info("Scaling Payload: %s payload")
-    scaler = StandardScaler().fit(payload)
+    scaler = StandardScaler(with_mean=False).fit(payload)
     scaled_adhoc_predict = scaler.transform(payload)
     return scaled_adhoc_predict
 
@@ -56,6 +57,7 @@ def predict():
 
     try:
         clf = joblib.load("boston_housing_prediction.joblib")
+        clf = clf[0][0]
     except:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
